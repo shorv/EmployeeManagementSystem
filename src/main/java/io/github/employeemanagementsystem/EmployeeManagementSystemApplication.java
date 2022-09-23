436152d48c7d598bff01fae504e05865bef679e2
shorv <kontakt.shorv@gmail.com>
2022-09-23 12:50:28 +0200

    initial commit

---
+++ src/main/java/io/github/employeemanagementsystem/EmployeeManagementSystemApplication.java
@@ -0,0 +1,22 @@
+package io.github.employeemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api/v1/helloworld")
+public class EmployeeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+    }
+
+    @GetMapping
+    public String helloWorld(){
+        return "HelloWorld";
+    }
+}
